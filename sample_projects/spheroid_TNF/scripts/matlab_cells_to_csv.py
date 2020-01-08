203ecd110e5c121e94c92106d2a0f3fcf108c9bc
Gerard Pradas <gerard.pradas@bsc.es>
2020-01-08 15:00:21 +0100

    script to convert files .mat to .csv accepts foler

--- sample_projects/spheroid_TNF/scripts/matlab_cells_to_csv.py
+++ sample_projects/spheroid_TNF/scripts/matlab_cells_to_csv.py
@@ -2,26 +2,38 @@ from scipy.io import loadmat
 import pandas as pd
 import argparse
 import os
+from pathlib import Path
+
+def convert_file(source, destination=""):
+    matlab_file = loadmat(source)
+    key = ""
+    if source.endswith("cells_physicell.mat"):
+        key = "cells"
+    elif source.endswith("cells.mat"):
+        key = "basic_agents"
+    elif source.endswith("microenvironment0.mat"):
+        key = "multiscale_microenvironment"
+    elif source.endswith("mesh0.mat"):
+        key = "mesh"
+    data = pd.DataFrame(matlab_file[key])
+    data = data.transpose()
+    data.to_csv(os.path.join(os.path.dirname(source), destination, "%s.csv" %os.path.splitext(os.path.basename(source))[0]))
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="This programs creates a macrocomplex given the pair of interactions.")
+    parser = argparse.ArgumentParser(description="Script to change from matlab to csv")
     parser.add_argument('-i', '--input',
                         dest="input",
                         action="store",
                         required=True,
-                        help="The input folder containing the complexes files")
-    parser.add_argument('-o', '--output',
-                        dest="output",
-                        action="store",
-                        default=".output/",
-                        help="The output file")
+                        help="Input file/folder containing matlab files")
 
     options = parser.parse_args()
-
-    if os.path.isfile(os.path.abspath(options.input)):
-        cells_physicell = loadmat(os.path.abspath(options.input))
-        data = pd.DataFrame(cells_physicell['cells'])
-        data = data.transpose()
-        data.to_csv("./output/%s.csv" %os.path.splitext(os.path.basename(os.path.abspath(options.input)))[0])
+    source = os.path.abspath(options.input)
+    if os.path.isfile(source):
+        convert_file(source)
     else:
-        pass
+        Path(source+"/csv/").mkdir(parents=True, exist_ok=True)
+        for file in os.listdir(source):
+            if file.endswith(".mat"):
+                file = os.path.join(source, file)
+                convert_file(file, "csv")
